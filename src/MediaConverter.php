43745bed1d4ee1b1134cc1824b05622b2b3e9bb7
wapmorgan <wapmorgan@gmail.com>
2016-03-21 08:30:31 -0800

    Update MediaConverter.php

--- src/MediaConverter.php
+++ src/MediaConverter.php
@@ -2,4 +2,39 @@
 
 namespace wapmorgan\MediaConverter;
 
-class MediaConverter {}
+class MediaConverter {
+
+    public $filename;
+    public $encoder;
+
+    public function __construct($filename) {
+        $this->filename = $filename;
+        if (($bin = find_existing_file(['/usr/bin/ffmpeg', '/usr/local/bin/ffmpeg'])) !== false)
+            $this->encoder = new Encoders\Ffmpeg($bin);
+        else
+            throw new \Extension('No available encoders in your system. Please visit http://github.com/wapmorgan/MediaConverter/README.md to solve this problem.');
+    }
+
+    public function convertTo3gp($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, []);
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function getIsAudio() {
+        $streams = $this->encoder->getStreams($this->filename);
+        return count($streams) == 1 && $streams[0]->type == 'audio';
+    }
+
+    public function getIsVideo() {
+        return !$this->getIsAudio();
+    }
+
+}
