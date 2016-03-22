f1f3a20388a512fdb11e979081f75045f503d732
wapmorgan <wapmorgan@gmail.com>
2016-03-22 08:34:39 -0800

    Update Ffmpeg.php

--- src/Encoders/Ffmpeg.php
+++ src/Encoders/Ffmpeg.php
@@ -2,4 +2,24 @@
 
 namespace wapmorgan\MediaConverter\Encoders;
 
-class Ffmpeg {}
+class Ffmpeg {
+
+    public $bin;
+
+    public function __construct($bin) {
+        $this->bin = $bin;
+    }
+
+    public function convert($input, $output, $options) {
+        $cmd = $this->bin.' -i '.escapeshellarg($input);
+        foreach ($options as $option => $value) {
+            $cmd .= ' -'.$option.' '.escapeshellarg($value)};
+        }
+        $cmd .= ' '.$output;
+
+        $result = system($cmd);
+
+        return $result;
+    }
+
+}
