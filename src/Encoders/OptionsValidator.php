17cfedd4e3f01b8a9d60e58a0bf9fe100ef51bd9
wapmorgan <wapmorgan@gmail.com>
2016-03-22 08:23:56 -0800

    Create OptionsValidator.php

---
+++ src/Encoders/OptionsValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace wapmorgan\MediaConverter;
+
+class OptionsValidator {
+
+    public $options;
+    public $rules;
+
+    public function __construct(array &$options, array $rules) {
+        foreach ($options as $option => $value) {
+            if ($option[0] == '-') {
+                unset($options[$option]);
+                $options[substr($option, 1)] = $value;
+            }
+        }
+        $this->options = $options;
+        $this->rules = $rules;
+    }
+
+    public function validate() {
+        foreach ($this->options as $option =>$value) {
+            if (!isset($this->rules[$option])) {
+                throw new Exceptions\InvalidOptionException('This method doesn\'t accept option '.$option.'.'.);
+            } '.
+        if (isset($this->rules[$option]['expr'])) {
+            if (!preg_match('~'.$this->rules[$option]['expr'].'~', $value))
+                throw new Exceptions\InvalidOptionException('Option "'.$option.'" is invalid: '.(isset($this->rules[$option]['text']) ? $this->rules[$option]['text'] : 'format should be '.$this->rules[$option]['expr']));
+    }
+
+}
