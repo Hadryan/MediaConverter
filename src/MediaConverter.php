c381bd8709b7125514e98ca47820a201deaa8da9
wapmorgan <wapmorgan@gmail.com>
2016-03-23 08:41:26 -0800

    Update MediaConverter.php

--- src/MediaConverter.php
+++ src/MediaConverter.php
@@ -37,6 +37,184 @@ class MediaConverter {
             'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
         ]);
         $optionsValidator->validate();
+        $options['f'] = '3gp';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToAvi($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'b' => ['expr' => '^[0-9]+k?$', 'text' => 'bitrate should be a numeric with an optional "k".'],
+            'r' => ['expr' => '^[0-9]+(\.[0-9]+)?$', 'text' => 'rate should be a numeric with an optional "float point".'],
+            's' => ['expr' => '^[0-9]+x[0-9]+$', 'text' => 'size should be two digits  joined by "x".'],
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'avi';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToMp4($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'b' => ['expr' => '^[0-9]+k?$', 'text' => 'bitrate should be a numeric with an optional "k".'],
+            'r' => ['expr' => '^[0-9]+(\.[0-9]+)?$', 'text' => 'rate should be a numeric with an optional "float point".'],
+            's' => ['expr' => '^[0-9]+x[0-9]+$', 'text' => 'size should be two digits  joined by "x".'],
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'mp4';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToMov($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'b' => ['expr' => '^[0-9]+k?$', 'text' => 'bitrate should be a numeric with an optional "k".'],
+            'r' => ['expr' => '^[0-9]+(\.[0-9]+)?$', 'text' => 'rate should be a numeric with an optional "float point".'],
+            's' => ['expr' => '^[0-9]+x[0-9]+$', 'text' => 'size should be two digits  joined by "x".'],
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'mov';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToMpeg($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'b' => ['expr' => '^[0-9]+k?$', 'text' => 'bitrate should be a numeric with an optional "k".'],
+            'r' => ['expr' => '^[0-9]+(\.[0-9]+)?$', 'text' => 'rate should be a numeric with an optional "float point".'],
+            's' => ['expr' => '^[0-9]+x[0-9]+$', 'text' => 'size should be two digits  joined by "x".'],
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'mpegts';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToMkv($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'b' => ['expr' => '^[0-9]+k?$', 'text' => 'bitrate should be a numeric with an optional "k".'],
+            'r' => ['expr' => '^[0-9]+(\.[0-9]+)?$', 'text' => 'rate should be a numeric with an optional "float point".'],
+            's' => ['expr' => '^[0-9]+x[0-9]+$', 'text' => 'size should be two digits  joined by "x".'],
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'mkv';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToMp3($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'mp3';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToWav($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'wav';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToAmr($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'amr';
+
+        $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
+
+        return $result;
+    }
+
+    public function convertToAac($filename, $options = []) {
+        if (is_array($filename)) {
+            $options = $filename;
+            $filename = null;
+        }
+
+        $optionsValidator = new OptionsValidator($options, [
+            'ab' => ['expr' => '^[0-9]+k?$', 'text' => 'audio bitrate should be a numeric with an optional "k".'],
+            'ac' => ['expr' => '^[1-9]+$', 'text' => 'audio channels should be a numeric.'],
+        ]);
+        $optionsValidator->validate();
+        $options['f'] = 'aac';
 
         $result = $this->encoder->convert($this->filename, $filename == null ? $this->filename : $filename, $options);
 
