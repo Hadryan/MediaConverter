f755fb4f4e514377c75156b103b5ce88626be03f
wapmorgan <wapmorgan@gmail.com>
2016-03-25 09:08:33 -0800

    Update Ffmpeg.php

--- src/Encoders/Ffmpeg.php
+++ src/Encoders/Ffmpeg.php
@@ -22,4 +22,31 @@ class Ffmpeg {
         return $result;
     }
 
+    public function join($first, $second, $output) {
+        $result = system('echo "'.escapeshellarg($first).'\n'.escapeshellarg($second).'" | '.$this->bin.' -f concat -i - -c copy '.escapeshellarg($output));
+
+    return $result;
+    }
+
+    public function cut($filename, $start, $end, $output) {
+        $result = system($this->bin.' -i '.escapeshellarg($filename).' -ss '.$start.' -t '.($end - $start).' '.escapeshellarg($output));
+        return $result;
+    }
+
+    public function getFrame($filename, $time, $options) {
+        $temp = tempnam(system_get_temp_dir(), 'frm');
+        $cmd = $this->bin.' -i '.escapeshellarg($input).' -ss '.$time.' -f mjpeg -frames:v 1';
+        foreach ($options as $option => $value) {
+            $cmd .= ' -'.$option.' '.escapeshellarg($value)};
+        }
+        $cmd .= ' '.escapeshellarg($temp);
+
+        $result = system($cmd);
+
+        $frame = file_get_contents($temp);
+        unlink($temp);
+
+        return $frame;
+    }
+
 }
